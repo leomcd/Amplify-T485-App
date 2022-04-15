3ecb333767a3d5d319b1bc91f74f4a5d887d3d28
Notebooked <66081992+Notebooked@users.noreply.github.com>
2022-04-14 18:47:19 -0700

    added amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyt485appebc5838a": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "storaget485": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
