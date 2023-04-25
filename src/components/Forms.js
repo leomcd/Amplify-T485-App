21557d369e0743cf0fc739688af1e394c565ff1f
Notebooked <66081992+Notebooked@users.noreply.github.com>
2023-04-24 17:58:53 -0700

    new form

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -18,6 +18,10 @@ function Forms() {
             <h2 className="form-name">Eagle Scout Service Project Workbook</h2>
             <a className="btn btn-dark form-link" target="_blank" rel="noreferrer" href="https://www.scouting.org/programs/scouts-bsa/advancement-and-awards/eagle-scout-workbook/">Link</a>
           </div>
+          <div className="form">
+            <h2 className="form-name">Eagle Scout Service Project Workbook</h2>
+            <a className="btn btn-dark form-link" target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1_2wIO2ho_GV3n4UlGEwFr03wz0-5wac-Knk_RBkBLS0/edit?usp=sharing">Link</a>
+          </div>
         </div>
       </WithAuth>
     </div>
